eb8920c583092efd877825282437f4aef1af4468
yashkataraei <yashkataraei@gmail.com>
2025-08-13 12:00:03 +0530

    context api discussion

---
+++ context-Api/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Login from './components/Login'
+import Profile from './components/Profile'
+import UserContextProvider from './context/UserContextProvider'
+
+function App() {
+  
+
+  return (
+    <UserContextProvider>
+      
+     <h1>React with Chai </h1>
+     <Login/>
+      <Profile/>
+      
+      
+    </UserContextProvider>
+  )
+}
+
+export default App
