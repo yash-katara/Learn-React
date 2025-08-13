c244f0d88883e05bc8e9b0d647912826b6491271
yashkataraei <yashkataraei@gmail.com>
2025-08-13 13:01:15 +0530

    yesss

--- themeswitcher/src/App.jsx
+++ themeswitcher/src/App.jsx
@@ -1,14 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <h1>hello</h1>
+      <h1 className='p-4 bg-pink-500 text-3xl'>hello</h1>
+      <div className="flex flex-wrap min-h-screen items-center">
+                <div className="w-full">
+                    <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                        
+                    </div>
+
+                    <div className="w-full max-w-sm mx-auto">
+                       
+                    </div>
+                </div>
+            </div>
     </>
   )
 }
